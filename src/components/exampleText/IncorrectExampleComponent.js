30991b1f268ee3848ad0d36c7db66c8a1d742bc6
Reuben Ellis <ethriel3695@gmail.com>
2017-11-18 11:30:05 -0700

    modified the example object to an example array

--- src/components/exampleText/IncorrectExampleComponent.js
+++ src/components/exampleText/IncorrectExampleComponent.js
@@ -5,11 +5,11 @@ class IncorrectExampleComponent extends React.Component {
   render () {
     return (
       <div className={codeContent}>
-        <span>{`{`}</span><br />
+        <span>{`[`}</span><br />
         <span>&emsp;{`"react: react-router-dom"`}</span><br />
         <span>&emsp;{`"react-dom: react"`}</span><br />
         <span>&emsp;{`"react-router-dom: react"`}</span><br />
-        <span>{`}`}</span>
+        <span>{`]`}</span>
       </div>
     );
   }
