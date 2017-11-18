11c21a8b8ae2a746ae3b285783237c5eb9580b61
Reuben Ellis <ethriel3695@gmail.com>
2017-11-18 07:13:00 -0700

    added test files and app skeleton

---
+++ src/index.js
@@ -0,0 +1,9 @@
+/* eslint-disable import/default */
+import React from 'react';
+import { render } from 'react-dom';
+import App from './components';
+
+render(
+  <App />,
+  document.getElementById('app')
+);
---
+++ src/index.test.js
@@ -0,0 +1,7 @@
+import expect from 'expect';
+
+describe('This test verifies the testing engine is working', () => {
+  it('should pass', () => {
+    expect(true).toEqual(true);
+  });
+});
